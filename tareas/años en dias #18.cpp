3c8f1debca348dfc6a33b611ed3ac72ea9d81372
Matias Celiz <82237565+Celiz@users.noreply.github.com>
2021-06-10 08:16:29 -0300

    Rename años en dias.cpp to años en dias #18.cpp

--- tareas/años en dias #18.cpp
+++ tareas/años en dias #18.cpp
@@ -16,7 +16,7 @@ cumple(){
 	anios=dias/365;
 	meses=(dias-(anios*365))/30;
 	dias=dias-anios*365-(meses*30);
-	(anios > 1) ? cout<< anios << " años\n" : cout<<anios<<" año\n";
+	(anios > 1) ? cout<< anios << " aÃ±os\n" : cout<<anios<<" aÃ±o\n";
 	(meses > 1) ? cout<<meses<< " meses\n" : cout<<meses<<" mes\n";
 	(dias > 1) ? cout<<dias<< " dias\n" : cout<<anios<<" dia\n";
 	
