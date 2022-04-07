e523cc5908ac392e8a88f93bd63c8a3663bb3615
Matias Celiz <82237565+Celiz@users.noreply.github.com>
2022-04-07 14:51:48 -0300

    Add files via upload

---
+++ 5to/JavaScript/año_bisiesto/ab.js
@@ -0,0 +1,7 @@
+let anio = prompt("Ingrese el año");
+if(anio % 4 == 0 || anio % 400 == 0 || anio % 100 == 0){
+  
+            document.write('El año ' +anio+ ' es bisiesto');	
+        }else{
+            document.write('El año ' +anio+ ' no es bisiesto');
+        }       
