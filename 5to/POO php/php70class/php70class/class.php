0c2a84a89867938cbd4f2d566cf7b494e16713f3
Matias Celiz <82237565+Celiz@users.noreply.github.com>
2022-07-05 14:26:09 -0300

    Add files via upload

---
+++ 5to/POO php/php70class/php70class/class.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<title>Pruebas</title>
+</head>
+<body>
+<?php
+class Empleado {
+  private $nombre;
+  private $horas;
+  private $sueldoH;
+  function __construct($nom,$hor,$suel){
+    $this->nombre = $nom;
+    $this->horas = $hor;
+    $this->sueldoH = $suel;
+  }
+
+  public function getNombre()
+  {
+    return $this->nombre;
+  }
+  public function getHoras()
+  {
+    return $this->horas;
+  }
+  public function getSueldoH()
+  {
+    return $this->sueldoH;
+  }
+ 
+  public function calcularSueldo()
+  {
+    return $this->horas * $this->sueldoH;
+  }
+}
+$empleado = new Empleado($_POST['name'], $_POST['hours'], $_POST['salary']);
+echo "Nombre: " . $empleado->getNombre() . "<br>";
+echo "Horas: " . $empleado->getHoras() . "<br>";
+echo "Sueldo por hora: " . $empleado->getSueldoH() . "<br>";
+echo "Sueldo total: " . $empleado->calcularSueldo() . "<br>";
+?>
+</body>
+</html>
