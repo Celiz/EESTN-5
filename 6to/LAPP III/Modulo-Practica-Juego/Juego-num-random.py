08c9e9e610050fd700a9c0d2d2a72f2ea6f5d075
Matias Celiz <82237565+Celiz@users.noreply.github.com>
2023-05-17 13:15:38 -0300

    Update Juego-num-random.py

--- 6to/LAPP III/Modulo-Practica-Juego/Juego-num-random.py
+++ 6to/LAPP III/Modulo-Practica-Juego/Juego-num-random.py
@@ -1,15 +1,15 @@
 import random
 def adivina_numero():
-    numero_secreto = random.randint(0, 100)
+    numero = random.randint(0, 100)
     intentos = 0
 
     while True:
         intento = int(input("Adivina el número (entre 0 y 99): "))
         intentos += 1
 
-        if intento < numero_secreto:
+        if intento < numero:
             print("Te quedaste corto. Intenta nuevamente.")
-        elif intento > numero_secreto:
+        elif intento > numero:
             print("Te pasaste. Intenta nuevamente.")
         else:
             print("¡Felicidades! Adivinaste el numero")
