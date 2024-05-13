b93d31c0ffba06a7f65e6fde56f2d2cd0171b6d0
Celiz <maticelizramos@gmail.com>
2024-05-13 18:49:32 -0300

    tuki

--- 7mo/PDIS/nodejs10/js/main.js
+++ 7mo/PDIS/nodejs10/js/main.js
@@ -24,8 +24,8 @@ class CJugadores {
           <td>${jugador.name}</td>
           <td>${jugador.position}</td>
           <td>${jugador.age}</td>
-          <td>${jugador.height}</td>
-          <td>${jugador.weight}</td>
+          <td>${jugador.height} m</td>
+          <td>${jugador.weight} kg</td>
           <td>${jugador.nacionality}</td>
         `;
         tablaBody.appendChild(fila);
@@ -34,66 +34,62 @@ class CJugadores {
   
     listARgentinaNacionality() {
       let count = 0;
-      for (const person of this.person) {
-        if (person.nacionality === 'Argentino') {
+      for (const jugador of this.jugador) {
+        if (jugador.nacionality == 'Argentina') {
           count++;
         }
       }
       return count;
     }
   
-    between1960and1980() {
+    //Listar todos los jugadores que tengan peso entre 75 y 80 kg.
+    between75and80kg() {
       let count = 0;
-      for (const person of this.person) {
-        if (person.yearPublished >= 1960 && person.yearPublished <= 1980) {
+      for (const jugador of this.jugador) {
+        console.log(jugador.weight);
+        if (jugador.weight >= 75 && jugador.weight <= 80) {
           count++;
         }
       }
       return count;
-    }
-  
-    averageAgePublished() {
-      let sum = 0;
-      for (const person of this.person) {
-        sum += person.agePublished;
-      }
-      return sum / this.person.length;
     }
 
-    showPerson() {
-      let html = '';
-      for (const person of this.person) {
-        html += `
-          
-            ${person.name}
-            ${person.surname}
-            ,
-        `;
+    //Mostrar el jugador más alto.
+    playerTall() {
+      let max = 0;
+      let name = '';
+      for (const jugador of this.jugador) {
+        if (jugador.height > max) {
+          max = jugador.height;
+          name = jugador.name;
+        }
       }
-      return html;
+      return name;
     }
+    
+      
+
+
+
 
   }
-  const person = new CPerson();
+  const jugador = new CJugadores();
 
-  person.agregarJugador('Fábio1', 'G', 43, '1.88 m', '86 kg', 'Brasil');
-  person.agregarJugador('Marlon4', 'D', 28, '1.83 m', '78 kg', 'Brasil');
-  person.agregarJugador('Marcelo12', 'D', 35, '1.73 m', '72 kg', 'Brasil');
-  person.agregarJugador('Nino33', 'D', 26, '1.88 m', '82 kg', 'Brasil');
-  person.agregarJugador('John Kennedy9', 'A', 21, '1.73 m', '71 kg', 'Brasil');
-  person.agregarJugador('Keno11', 'A', 34, '1.78 m', '72 kg', 'Brasil');
-  person.agregarJugador('Germán Cano14', 'A', 35, '1.78 m', '81 kg', 'Argentina');
+  jugador.agregarJugador('Fábio1', 'G', 43, 1.88 , 86 , 'Brasil');
+  jugador.agregarJugador('Marlon4', 'D', 28, 1.83 , 78, 'Brasil');
+  jugador.agregarJugador('Marcelo12', 'D', 35, 1.73 , 72 , 'Brasil');
+  jugador.agregarJugador('Nino33', 'D', 26, 1.88 , 82 , 'Brasil');
+  jugador.agregarJugador('John Kennedy9', 'A', 21, 1.73 , 71, 'Brasil');
+  jugador.agregarJugador('Keno11', 'A', 34, 1.78 , 72, 'Brasil');
+  jugador.agregarJugador('Germán Cano14', 'A', 35, 1.78 , 81 , 'Argentina');
 
   // Generar la tabla de animales
-  person.generarTablaPersonas();
+  jugador.generarTablaPersonas();
 
-  document.getElementById("resultado1").textContent = "Listar todos los autores de que sean de Nacionalidad Argentina: " + person.listARgentinaNacionality();
+  document.getElementById("resultado1").textContent = "Listar todos los jugadores que sean de Nacionalidad Argentina: " + jugador.listARgentinaNacionality();
   
-  document.getElementById("resultado2").textContent = "Listar todos los autores que publicaron entre 1960 a 1980: " + person.between1960and1980();
+  document.getElementById("resultado2").textContent = "Listar todos los jugadores que tengan peso entre 75 y 80 kg: " + jugador.between75and80kg();
   
+  document.getElementById("resultado3").textContent = "Mostrar el jugador más alto: " + jugador.playerTall();
 
-  document.getElementById("resultado3").textContent = "Mostrar el promedio de la edad de publicación: " + person.averageAgePublished();
-
-  //mostrar a los autores
-  document.getElementById("resultado4").textContent = "Mostrar todos los autores: " + person.showPerson();
   
